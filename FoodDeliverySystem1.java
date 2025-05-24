8292c5f5efc2dd0cf4fc581584105ca2747a4024
Lyprandos Liatsos <102907631+LyprandosLia@users.noreply.github.com>
2025-05-24 23:00:04 +0300

    Add files via upload

---
+++ FoodDeliverySystem1.java
@@ -0,0 +1,28 @@
+package texnologia.logismikou;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import texnologia.logismikou.Login;
+import java.sql.DriverManager;    
+import java.sql.Statement;         
+import java.sql.ResultSet;         
+import java.sql.SQLException; 
+
+
+
+
+
+public class FoodDeliverySystem1{
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        
+     
+              Login frame = new Login();
+            frame.setVisible(true);
+   
+            System.out.println("Cool");
+    }
+           
+
+}
